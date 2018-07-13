3893ea3a7e930eba4c64ac3060dc6977d44ce65f
amber wade <amberwade@ambers-MacBook-Pro.local>
2018-07-13 04:51:14 -0700

    deployfix

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
+
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
+
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
 
-// Initialize Express
 var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
+ 
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var databaseUri = "mongodb://localhost/scraper28Db";
+if(process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else{
+  mongoose.connect(databaseUri);
+}
+var Connect = mongoose.connection;
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+Connect.on('error',function(err){
+  console.log("Mongoose error: ",err);
+})
+
+
 
 
 // Routes
